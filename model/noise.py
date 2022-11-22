54f639020963384cd53c0fa4ec27f9d1a949dad6
Aibek Bekbayev <aybek9221@gmail.com>
2022-11-22 16:51:21 +0000

    add get emb for timestamp

--- model/noise.py
+++ model/noise.py
@@ -1,10 +1,19 @@
 import torch 
+import math
 
 class NoiseScheduler:
     def __init__(self, c_r, max_positions=10000) -> None:
         self.c_r = c_r
         self.max_positions = max_positions
         self.num_tokens = 8192
+    def get_embedding(self, r):
+        half_dim = self.c_r // 2
+        r = self.gamma(r) * self.max_positions
+        emb = math.log(self.max_positions) / (half_dim - 1)
+        emb = torch.arange(half_dim, device=r.device).float().mul(-emb).exp()
+        emb = r[:, None]*emb[None, :]
+        emb = torch.cat([emb.cos(), emb.sin()], dim=-1)
+        return emb.to(r.dtype) 
     def gamma(self, r):
         return (r * torch.pi / 2).cos()
     def get_noise(self, r, x):
